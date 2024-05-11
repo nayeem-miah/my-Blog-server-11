7edee57e5523cffe9a188297f7cf09bcd1d27c1e
nayeem-miah <nayeem5113a@gmail.com>
2024-05-11 13:12:04 +0600

    allBlogs

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ app.use(
 );
 app.use(express.json());
 // app.use(cookieParser());
-console.log(process.env.DB_NAME);
-console.log(process.env.DB_PASS);
+// console.log(process.env.DB_NAME);
+// console.log(process.env.DB_PASS);
 // ----------------------------------------------
 
 
@@ -38,6 +38,21 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+    const blogsCollection = client.db('blogsDB').collection('blogs');
+
+    app.post('/blogs', async(req, res)=>{
+      const newBlogs = req.body;
+      console.log(newBlogs);
+      const result =await blogsCollection.insertOne(newBlogs);
+      res.send(result);
+
+    })
+    app.get('/blogs', async(req, res)=>{
+      const result = await blogsCollection.find().toArray();
+      res.send(result)
+    })
+
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
