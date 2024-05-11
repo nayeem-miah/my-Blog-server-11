651834a62c51f53f1fb07c829ec29815406e7d1f
nayeem-miah <nayeem5113a@gmail.com>
2024-05-11 16:21:27 +0600

    added-recentBlog

--- index.js
+++ index.js
@@ -2,17 +2,14 @@ const express = require("express");
 const cors = require("cors");
 // const jwt = require("jsonwebtoken");
 // const cookieParser = require("cookie-parser");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 //middleware
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      
-    ],
+    origin: ["http://localhost:5173"],
     credentials: true,
   })
 );
@@ -22,7 +19,6 @@ app.use(express.json());
 // console.log(process.env.DB_PASS);
 // ----------------------------------------------
 
-
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.bomlehy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,31 +27,50 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
-    const blogsCollection = client.db('blogsDB').collection('blogs');
+    const blogsCollection = client.db("blogsDB").collection("blogs");
+    const recentBlogsCollection = client.db("blogsDB").collection("recent");
 
-    app.post('/blogs', async(req, res)=>{
+    // recent blogs
+    app.get("/recent", async (req, res) => {
+      const result = await recentBlogsCollection.find().toArray();
+      res.send(result);
+    });
+    // app.get("/recentDetails/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: new ObjectId(id) };
+    //   const result = await recentBlogsCollection.findOne(query);
+    //   res.send(result);
+    // });
+
+    app.post("/blogs", async (req, res) => {
       const newBlogs = req.body;
       console.log(newBlogs);
-      const result =await blogsCollection.insertOne(newBlogs);
+      const result = await blogsCollection.insertOne(newBlogs);
       res.send(result);
-
-    })
-    app.get('/blogs', async(req, res)=>{
+    });
+    app.get("/blogs", async (req, res) => {
       const result = await blogsCollection.find().toArray();
-      res.send(result)
-    })
-
+      res.send(result);
+    });
+    app.get("/blogs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await blogsCollection.findOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -63,36 +78,11 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
 // --------------------------------------------------
 
-
-
 app.get("/", (req, res) => {
-    res.send("My BlogIng server is Running");
-  });
-  app.listen(port, () => {
-    console.log(`My BlogIng server is Running on port ${port}`);
-  });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  res.send("My BlogIng server is Running");
+});
+app.listen(port, () => {
+  console.log(`My BlogIng server is Running on port ${port}`);
+});
