60d6a17cf410fd26d0bc483cbf67f710009ae98b
nayeem-miah <nayeem5113a@gmail.com>
2024-05-13 16:53:56 +0600

    fetureBlogs

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-// const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 // const cookieParser = require("cookie-parser");
+const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
@@ -14,7 +15,8 @@ app.use(
   })
 );
 app.use(express.json());
-// app.use(cookieParser());
+app.use(cookieParser());
+
 // console.log(process.env.DB_NAME);
 // console.log(process.env.DB_PASS);
 // ----------------------------------------------
@@ -29,14 +31,53 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+//  middleware
+const logger = (req, res, next) => {
+  console.log("LOGGED INFFPO", req.method, req.ulr);
+  next();
+};
 
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token;
+  //  no token available
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     const blogsCollection = client.db("blogsDB").collection("blogs");
     const recentBlogsCollection = client.db("blogsDB").collection("recent");
-
+    const wishListCollection = client.db("blogsDB").collection("wishList");
+    // jwt
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      // console.log("user for ", user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "none",
+        })
+        .send({ success: true });
+    });
+    app.post("/logout", async (req, res) => {
+      const user = req.body;
+      console.log("log in out ", user);
+      res.clearCookie("token", { maxAge: 0 }).send({ success: true });
+    });
     // recent blogs
     app.get("/recent", async (req, res) => {
       const result = await recentBlogsCollection.find().toArray();
@@ -44,6 +85,7 @@ async function run() {
     });
     app.get("/recentDetails/:id", async (req, res) => {
       const id = req.params.id;
+
       const query = { _id: new ObjectId(id) };
       const result = await recentBlogsCollection.findOne(query);
       res.send(result);
@@ -51,7 +93,7 @@ async function run() {
 
     app.post("/blogs", async (req, res) => {
       const newBlogs = req.body;
-      console.log(newBlogs);
+      // console.log(newBlogs);
       const result = await blogsCollection.insertOne(newBlogs);
       res.send(result);
     });
@@ -92,29 +134,52 @@ async function run() {
     });
     // -----------------------------------
     // featuredBlogs
-    app.get("/featuredBlogs/:email", async (req, res) => {
-      const result = await blogsCollection
-        .find({ email: req.params.email })
-        .toArray();
-      res.send(result);
-    });
-    // delete 
-    app.delete('/featuredBlogs/:id', async(req, res)=>{
-      const id = req.params.id;
-      const query = {_id : new ObjectId(id)}
-      const result = await blogsCollection.deleteOne(query);
-      res.send(result);
-    })
-
-    
-    
-    // app.delete("/myList/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: new ObjectId(id) };
-    //   const result = await touristCollection.deleteOne(query);
+    // app.get("/featuredBlogs/:email", logger, verifyToken, async (req, res) => {
+    //   // console.log("owner info", res.user);
+    //   // if(req.user.email !== req.query.email){
+    //   //   return res.status(403).send({message : "forbidden access"})
+    //   // }
+    //   let query = {};
+    //   if(req.query?.email){
+    //     query = {email : req.query.email}
+    //   }
+    //   const result = await blogsCollection
+    //     .find(query)
+    //     .toArray();
     //   res.send(result);
     // });
 
+    app.get("/featuredBlogs", async (req, res) => {
+      const result = await blogsCollection.find().toArray();
+      res.send(result);
+    });
+
+    // delete
+    app.delete("/featuredBlogs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await blogsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // -------------wish list ------------------
+    app.post("/wishlist", async (req, res) => {
+      const wishlist = req.body;
+      console.log(wishlist);
+      const result = await wishListCollection.insertOne(wishlist);
+      res.send(result);
+    });
+    // app.get('/wishlist/:email', async(req, res)=>{
+    //   const email = req.params.email;
+    //   const query = {_id: new ObjectId(email)}
+    //   const result = await wishListCollection.find(query).toArray();
+    //   res.send(result);
+    // })
+
+    app.get("/wishlist", async (req, res) => {
+      const result = await wishListCollection.find().toArray();
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log(
