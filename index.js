7eddb3ca10671a1f7114a2eb4b3f207193eaf1ee
nayeem-miah <nayeem5113a@gmail.com>
2024-05-15 16:18:05 +0600

    deploy

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://my-personal-blog-fcd20.web.app"],
     credentials: true,
   })
 );
@@ -51,6 +51,11 @@ const verifyToken = async (req, res, next) => {
     next();
   });
 };
+const cookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production" ? true: false,
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -66,18 +71,12 @@ async function run() {
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: "1h",
       });
-      res
-        .cookie("token", token, {
-          httpOnly: true,
-          secure: true,
-          sameSite: "none",
-        })
-        .send({ success: true });
+      res.cookie("token", token, cookieOptions).send({ success: true });
     });
     app.post("/logout", async (req, res) => {
       const user = req.body;
       console.log("log in out ", user);
-      res.clearCookie("token", { maxAge: 0 }).send({ success: true });
+      res.clearCookie("token", {...cookieOptions, maxAge: 0}).send({ success: true });
     });
     // recent blogs
     app.get("/recent", async (req, res) => {
@@ -98,7 +97,7 @@ async function run() {
       const result = await blogsCollection.insertOne(newBlogs);
       res.send(result);
     });
-    //  search and show blogs 
+    //  search and show blogs
     app.get("/blogs", async (req, res) => {
       const search = req.query.search;
       let query = {
